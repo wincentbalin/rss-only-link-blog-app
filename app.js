eb20443ac17fb964ab0ac17dbce307214a1e53a6
Wincent Balin <wincent.balin@gmail.com>
2024-08-09 01:16:27 +0200

    First steps
    The page shows input fields with submit button, as well as an iframe for viewing the blog. The app implements basic saving and loading of settings. It also checks against unencrypted connections and prevents you from using them,
    
    Caveat emptor: it saves password unencrypted!

---
+++ app.js
@@ -0,0 +1,39 @@
+function saveBlogUrlAndPassword() {
+    if (!('localStorage' in window)) {
+        alert('Your browser does not support saving blog parameters!');
+        return;
+    }
+
+    localStorage.blogUrl = document.querySelector('#blog-url').value;
+    localStorage.blogPassword = document.querySelector('#blog-password').value;
+}
+
+function loadBlogUrlAndPassword() {
+    if (!('localStorage' in window)) {
+        alert('Your browser does not support loading blog parameters!');
+        return;
+    }
+
+    var url = localStorage.blogUrl;
+    document.querySelector('#blog-url').value = url ? url : '';
+    var password = localStorage.blogPassword;
+    document.querySelector('#blog-password').value = password ? password : '';
+}
+
+document.querySelector('#blog-url').addEventListener('blur', (event) => {
+    document.querySelector('form').setAttribute('action', event.target.value);
+    saveBlogUrlAndPassword();
+});
+
+document.querySelector('#blog-password').addEventListener('blur', (event) => {
+    saveBlogUrlAndPassword();
+});
+
+document.querySelector('form').addEventListener('submit', (event) => {
+    event.preventDefault();
+    if (/^http:/.test(document.querySelector('form').action)) {
+        alert('The page uses insecure connection!\nPlease use an address starting with https://');
+        return false;
+    }
+    // TODO: Save URL and password 
+});
