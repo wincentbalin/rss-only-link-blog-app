d1b99bc135b9a0c0281dd05891d05d39b6138d49
Wincent Balin <wincent.balin@gmail.com>
2024-08-09 21:53:11 +0200

    Added service worker

---
+++ sw.js
@@ -0,0 +1,45 @@
+const VERSION = 'v1';
+const CACHE_NAME = `rss-only-link-blog-${VERSION}`;
+const APP_DIR = '/rss-only-link-blog-app';
+const APP_STATIC_RESOURCES = [
+    '/',
+    '/index.html',
+    '/style.css',
+    '/icon.svg',
+    '/app.js',
+    '/sw.js'
+].map((path) => {
+    return APP_DIR + path;
+});
+
+self.addEventListener('install', (event) => {
+    event.waitUntil((async () => {
+        const names = await caches.keys();
+        await Promise.all(names.map((name) => {
+            if (name !== CACHE_NAME) {
+                return caches.delete(name);
+            }
+        }));
+        await clients.claim();
+    })());
+});
+
+self.addEventListener('fetch', (event) => {
+    // Return index.html
+    if (event.request.mode === 'navigate') {
+        event.respondWith(caches.match(APP_DIR + '/'));
+        return;
+    }
+
+    // Return every other request
+    event.respondWith((async () => {
+        const cache = await caches.open(CACHE_NAME);
+        const cachedResponse = await cache.match(event.request.url);
+        // Return cached file
+        if (cachedResponse) {
+            return cachedResponse;
+        }
+        // Return 404
+        return new Response(null, {status: 404});
+    })());
+});
